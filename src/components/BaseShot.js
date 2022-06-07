e822f402bb3ae31096727991a0d595f7c5e2f826
BlueDream0718 <104906327+Cheng0718@users.noreply.github.com>
2022-06-07 08:19:39 -0800

    Update BaseShot.js

--- src/components/BaseShot.js
+++ src/components/BaseShot.js
@@ -16,7 +16,7 @@ let backAudio = loadSound('bMusic', true)
 backAudio.loop = true;
 backAudio.volume = 0.12;
 
-let currentSceneNumber = 25;
+let currentSceneNumber = 24;
 
 let loadImageCount = 0
 let wordVoiceList = [
